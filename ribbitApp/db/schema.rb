ff14c815285eaec0dc9b1082ee3534a45b989b4e
Martin Nicholson <martin.nicholson@gmail.com>
2013-03-01 16:23:02 +0000

    Ribbit functionality added

--- ribbitApp/db/schema.rb
+++ ribbitApp/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130221153442) do
+ActiveRecord::Schema.define(:version => 20130301160918) do
+
+  create_table "ribbits", :force => true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
