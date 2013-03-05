c40b3f2489e87a442daa8f815aa5b1193b3940d4
Martin Nicholson <martin.nicholson@gmail.com>
2013-03-05 13:33:01 +0000

    Following added

--- ribbitApp/app/controllers/users_controller.rb
+++ ribbitApp/app/controllers/users_controller.rb
@@ -24,6 +24,11 @@ class UsersController < ApplicationController
   def show
       @user = User.find(params[:id])
       @ribbit = Ribbit.new
+      
+      @relationship = Relationship.where(
+      	follower.id: current_user.id,
+      	followed_id: @user.id
+      ).first_or_initialize if current_user
   end
 
 
