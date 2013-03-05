b677ba82b677d0fa5f0212925fa30cd16f3ee03d
Martin Nicholson <martin.nicholson@gmail.com>
2013-03-05 13:23:04 +0000

    created user relationships

--- ribbitApp/app/models/user.rb
+++ ribbitApp/app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
   has_secure_password
   has_many :ribbits
+  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+  has_many :followers, through: :follower_relationships
+  has_many :followeds, through: :followed_relationships
 
   before_validation :prep_email
   before_save :create_avatar_url
@@ -17,6 +21,14 @@ class User < ActiveRecord::Base
 
   def create_avatar_url
       self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+  end
+
+  def following? user
+      self.followeds.include? user
+  end
+
+  def follow user
+      Relationship.create follower_id: self.id, followed_id: user.id
   end
 
 end
